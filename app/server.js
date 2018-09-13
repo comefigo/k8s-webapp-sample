34d88caa71d54750e8c16dcc1056deda95a84861
comefigo <comefigo@gmail.com>
2018-09-14 00:08:29 +0900

    add deployment_k8s_sample_app.yml add set / get value to redis

--- app/server.js
+++ app/server.js
@@ -1,11 +1,34 @@
 const http = require('http');
+const redis = require('redis');
+let redis_host = 'redis'
+if (process.env.MODE && process.env.MODE == 'k8s') {
+    redis_host = process.env.SAMPLE_REDIS_SERVICE_HOST;
+}
+const client = redis.createClient({
+    host: redis_host,
+    port: 6379
+});
+let redis_val = '';
+
+client.set('sample', 'hello world');
+client.set('sample2', 'hello Qiita');
+
+client.get('sample', function (err, data) {
+    redis_val += '<li>' + data + '</li>';
+});
+client.get('sample2', function (err, data) {
+    redis_val += '<li>' + data + '</li>';
+});
 
 const handleRequest = function (request, response) {
     console.log('Received request for URL: ' + request.url);
     response.writeHead(200);
-    const envs = process.env;
-    console.log(envs);
-    response.end('hello world v3');
+    response.end('<h1>redis value:</h1><ul>' + redis_val + '</ul>');
 };
+
+// echo env
+const envs = process.env;
+console.log(envs);
+
 const www = http.createServer(handleRequest);
-www.listen(8080);
+www.listen(3000);
