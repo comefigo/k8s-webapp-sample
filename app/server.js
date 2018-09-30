ea03ae66c3074777ddfc4029716c478b5bcd4753
comefigo <comefigo@gmail.com>
2018-09-30 16:13:22 +0900

    update

--- app/server.js
+++ app/server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const redis = require('redis');
 let redis_host = 'redis';
 let redis_port = 6379;
+
+// k8s環境の場合は、redisの接続先とポートは環境変数から取得
 if (process.env.MODE && process.env.MODE == 'k8s') {
     redis_host = process.env.SAMPLE_REDIS_SERVICE_HOST;
     redis_port = process.env.SAMPLE_REDIS_SERVICE_PORT;
@@ -10,11 +12,13 @@ const client = redis.createClient({
     host: redis_host,
     port: redis_port
 });
-let redis_val = '';
 
+// 表示用の変数をredisに保存する
 client.set('sample', 'hello world');
 client.set('sample2', 'hello Qiita');
 
+// 表示用の変数
+let redis_val = '';
 client.get('sample', function (err, data) {
     redis_val += '<li>' + data + '</li>';
 });
